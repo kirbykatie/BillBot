3ad0f4b3e40adc495ba9c1c24c97677d90dd2a16
Katie Blackstone <katierblackstone@gmail.com>
2023-10-13 12:46:36 -0500

    Making Bill more Bill-like. Aka, improving bot's responses

--- billbot.py
+++ billbot.py
@@ -1,6 +1,7 @@
+import math
 import os
+from random import random
 
-from apscheduler.jobstores.memory import MemoryJobStore
 from dotenv import load_dotenv
 import time
 from datetime import date, datetime, timedelta
@@ -9,7 +10,9 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 import discord
 from discord.ext import commands, tasks
+import arrow
 from parse_message import parse_message
+from bill_responses import reminderAcknowledgement
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -54,15 +57,19 @@ async def on_message(message):
     if 'remind me' in message.content.lower():
         parsed_reminder = parse_message(message.content)
         if parsed_reminder is not None:
-            await message.channel.send(f"Reminder {parsed_reminder.task} set at {time.strftime('%X (%d/%m/%y)')}")
+            print("Received reminder")
             scheduled_time = get_task_run_date(parsed_reminder.time_type, parsed_reminder.numeral)
             message_ref = message.to_reference()
             try:
+                current_time = arrow.utcnow().to('US/central');
                 sched.add_job(task, 'date', id=scheduled_time, run_date=scheduled_time, args=[parsed_reminder,
                                                                                               message_ref.message_id,
                                                                                               message_ref.channel_id,
-                                                                                              message_ref.guild_id])
-                print("Job made")
+                                                                                              message_ref.guild_id,
+                                                                                              current_time])
+                confirmation_response = get_confirmation_response(parsed_reminder.task, message.author.name, scheduled_time)
+                await message.channel.send(confirmation_response)
+                print("Reminder made")
             except Exception as e:
                 print(f"Error adding job: {e}")
     else:
@@ -76,11 +83,20 @@ def get_task_run_date(time_type, numeral):
     return formatted_datetime
 
 
-async def task(item, message_id, channel_id, guild_id):
+def get_confirmation_response(user_task, author, reminder_time):
+    random_res = reminderAcknowledgement[math.floor(random() * len(reminderAcknowledgement))]
+    random_res = random_res.replace("<$user$>", author)
+    humanized_reminder_time = arrow.get(reminder_time, tzinfo='US/central')
+    # humanized_reminder_time.to('-5:00')
+    print(humanized_reminder_time.humanize())
+    return f"{random_res} ({humanized_reminder_time.humanize()})"
+
+
+async def task(item, message_id, channel_id, guild_id, original_time):
     print(item.task, time.strftime('%X (%d/%m/%y)'))
     message_ref = discord.MessageReference(message_id=message_id, channel_id=channel_id, guild_id=guild_id)
     channel = bot.get_channel(channel_id)
-    await channel.send(f"{item.task}, {time.strftime('%X (%d/%m/%y)')}", reference=message_ref)
+    await channel.send(f"{item.task} (Reminder set on {original_time.format('MM/DD/YY hh:mm a')})", reference=message_ref)
 
 
 # Create an event loop and run the bot within it
